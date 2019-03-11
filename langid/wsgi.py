af7b53a0bd5fefdb626e6a5a264a1dc529feecd2
Jakub <jakub@datamaran.com>
2019-03-11 12:15:52 +0100

    fix response encoding

--- langid/wsgi.py
+++ langid/wsgi.py
@@ -2,4 +2,4 @@ from langid import application as langid_app
 
 def application(environ, start_response):
     res = langid_app(environ, start_response)
-    return res.encode('utf-8')
+    return [r.encode('utf-8') for r in res]
