4aec4d300064fe753ca48cd16b119d66ad5e2e58
Jakub <jakub@datamaran.com>
2019-03-11 12:13:03 +0100

    encode res

--- langid/wsgi.py
+++ langid/wsgi.py
@@ -1,5 +1,5 @@
 from langid import application as langid_app
 
 def application(environ, start_response):
-    [res] = langid_app(environ, start_response)
-    return [bytes(res, 'utf-8')]
+    res = langid_app(environ, start_response)
+    return res.encode('utf-8')
